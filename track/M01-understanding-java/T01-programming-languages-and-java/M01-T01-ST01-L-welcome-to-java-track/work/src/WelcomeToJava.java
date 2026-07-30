d3b7aa9196187ea04a3c3857df532a0a176d5fe8
Bitta Saiumesh <saiumeshbitta@gmail.com>
2026-07-30 12:45:23 +0530

    Day1changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,10 @@
+
 public class WelcomeToJava {
+
     public static void main(String[] args) {
-       //Write your code here
+        //Write your code here
+        System.out.println("Welcome to the java Track");
+        System.out.println("Iam already know to build programming logic");
+        System.out.println("Now iam learning how to express that logic using java.");
     }
 }
