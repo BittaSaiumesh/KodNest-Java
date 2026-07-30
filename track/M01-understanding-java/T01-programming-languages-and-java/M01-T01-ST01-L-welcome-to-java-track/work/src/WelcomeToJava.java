e9fd52525f68de18ad144138e9c680cfd4f926ed
Bitta Saiumesh <saiumeshbitta@gmail.com>
2026-07-30 12:59:41 +0530

    Added one more print statement

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -6,5 +6,6 @@ public class WelcomeToJava {
         System.out.println("Welcome to the java Track");
         System.out.println("Iam already know to build programming logic");
         System.out.println("Now iam learning how to express that logic using java.");
+        System.out.println("Now i know how to use git and github");
     }
 }
