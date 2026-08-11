c4b177f79853d6ebd0f9171846e8da454abea61f
Bitta Saiumesh <saiumeshbitta@gmail.com>
2026-08-11 11:08:33 +0530

    Emplyee Objects

---
+++ track/M02-introduction-to-java/ClassesAndObjects/EmpApplication.java
@@ -0,0 +1,28 @@
+class Employee{
+    int id;
+    String name;
+    void work(){
+        System.out.println(name+" is working");
+    }    
+}
+public class EmpApplication {
+    public static void main(String[] args) {
+        Employee e1=new Employee();
+        e1.id=10;
+        e1.name="Umesh";
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        e1.work();
+        Employee e2=new Employee();
+        e1=e2;
+        e2.id=20;
+        e2.name="jaanu";
+        System.out.println(e2.id);
+        System.out.println(e2.name);
+        e2.work();
+        System.out.println(e1.id);
+        System.out.println(e1.name);
+        e1.work();
+    }
+    
+}
--- track/M02-introduction-to-java/ClassesAndObjects/StudentObjects.java
+++ track/M02-introduction-to-java/ClassesAndObjects/StudentObjects.java
@@ -24,5 +24,29 @@ public class StudentObjects {
         System.out.println(s1.rollNo);
         s1.run();
         s1.sleep();
+        Student s2 = new Student();
+        s2.name="rani";
+        s2.age=20;
+        s2.height=5.7;
+        s2.rollNo=42;
+        System.out.println(s2.name);
+        System.out.println(s2.age);
+        System.out.println(s2.height);
+        System.out.println(s2.rollNo);
+        s2.run();
+        s2.sleep();
+
+        Student s3=new Student();
+        s3.name="suresh";
+        s3.age=20;
+        s3.height=5.7;
+        s3.rollNo=42;
+        System.out.println(s3.name);
+        System.out.println(s3.age);
+        System.out.println(s3.height);
+        System.out.println(s3.rollNo);
+        s3.run();
+        s3.sleep();
+        
     }
 }
